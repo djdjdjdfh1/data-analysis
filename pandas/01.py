788f307cf34e174083340f83df8084360471d8f0
Park Chan <qkrcks8028@gmail.com>
2025-05-19 20:01:48 +0900

    pandas 학습

--- pandas/01.py
+++ pandas/01.py
@@ -1,3 +1,9 @@
 # Pandas
 import pandas as pd
 # 1. Series
+temp = pd.Series([-20, -10, 10, 20])
+print(temp)
+
+temp = pd.Series([-20, -10, 10, 20], index=['Jan', 'Feb', 'Mar', 'Apr'])
+print(temp)
+print(temp['Jan'])
