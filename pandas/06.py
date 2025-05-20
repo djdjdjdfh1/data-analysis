6bd49f18d2d9e8ceb71c108091a6bc25b08cf189
Park Chan <qkrcks8028@gmail.com>
2025-05-20 21:16:43 +0900

    데이터 선택

--- pandas/06.py
+++ pandas/06.py
@@ -3,3 +3,10 @@ import pandas as pd
 df = pd.read_excel('score.xlsx', index_col='지원번호')
 # column 선택
 print(df[['이름', '키']])
+
+# COlumn 선택 (정수 index)
+print(df.columns[0])
+print(df.columns[2])
+print(df[df.columns[-1]])
+# 슬라이싱
+print(df[['이름','키']][:3])
---
+++ pandas/07.py
@@ -0,0 +1,3 @@
+# 데이터 선택 (loc)
+import pandas as pd
+df = pd.read_excel('score.xlsx', index_col='지원번호')
